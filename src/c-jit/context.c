a8de43a47c40afe8fdc17787849156d1e0f962e7
gripols <ilovethettc102@gmail.com>
2024-07-19 16:10:48 +0000

    blab

--- src/c-jit/context.c
+++
@@ -1,8 +0,0 @@
-#include "context.h"
-
-#include <string.h>
-
-void bus_context_init(struct bus_context *ctx, const unsigned int *program,
-		      FILE *in, FILE *out, void (*)(struct bus_context *))
-{
-}
